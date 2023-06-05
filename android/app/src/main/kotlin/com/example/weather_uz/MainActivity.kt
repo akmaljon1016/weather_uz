b93cacdb73dd0a15588ad9eff91c01eda85687c1
akmal1016 <akmaljonsotvoldiev1998@gmail.com>
2023-06-05 13:59:31 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/weather_uz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_uz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_uz/weather.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Dio dio = Dio();
+
+  Future<Weather> getWeather(String city) async {
+    var response = await dio.get("https://api.weatherapi.com/v1/forecast.json",
+        queryParameters: {
+          "key": "acb4a4de25aa41b784651422200510",
+          "days": "3",
+          "q": city
+        });
+    return Weather.fromJson(response.data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ob Havo"),
+      ),
+      body: FutureBuilder(
+        future: getWeather("Fergana"),
+          builder: (BuildContext context, AsyncSnapshot<Weather> snapshot) {
+        if (snapshot.hasData) {
+          return Center(
+            child: Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
+              color: Colors.blue),
+              width: 100,
+              height: 100,
+              child: Column(
+                children: [
+                  Image.network("https:"+snapshot.data!.current!.condition!.icon.toString()),
+                  Text(snapshot.data?.current?.tempC.toString() ?? "xato")
+                ],
+              ),
+            ),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      }),
+    );
+  }
+}
